00ae85a5ce79bfe8b37d87059f244772f2d4258e
wjc <49420645@qq.com>
2022-01-05 09:58:12 +0800

    判断提交类型

--- lib/utils/request.ts
+++ lib/utils/request.ts
@@ -1,18 +1,40 @@
 import request from 'superagent';
+import { QbitManage } from '../dto';
 /**
  * post 请求
  * @param env 环境变量
  * @param url
  * @param params
  * @param headers
+ * @param filenames 需要上传文件的key
  * @returns
  */
-export const postRequest = async (url: string, params: Record<string, any>, headers: Record<string, any> = {}): Promise<any> => {
+export const postRequest = async (
+  url: string,
+  params: Record<string, any>,
+  headers: Record<string, any> = {},
+  filenames: string[] = [],
+): Promise<any> => {
   try {
-    const result = await request
-      .post(url)
-      .send(params)
-      .set(headers);
+    let result: any;
+    if (headers['Content-Type'] === 'multipart/form-data') {
+      delete headers['Content-Type'];
+      const req = request.post(url).set(headers);
+      Object.keys(params).map((key: string) => {
+        if (filenames.includes(key)) {
+          req.attach(key, params[key]);
+        } else {
+          req.field(key, params[key]);
+        }
+      });
+      result = await req;
+    } else {
+      result = await request
+        .post(url)
+        .send(params)
+        .set(headers);
+    }
+
     return {
       httpStatus: result.status,
       ...result.body,
